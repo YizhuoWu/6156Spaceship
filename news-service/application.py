939f3b995c3d1a0d9cfeaf2412ce174b1c88c1f2
Yizhuo Wu <yizhuw1@uci.edu>
2021-10-29 16:07:43 -0400

    s

--- news-service/application.py
+++ news-service/application.py
@@ -1,25 +1,29 @@
-from flask import request, Flask, jsonify
-import os
+from flask import Flask, Response,request, jsonify
+from flask_cors import CORS
+from Comment_Application.RDBService import RDBService as RDBService
+import json
+import logging
+
 application = Flask(__name__)
+CORS(app)
+
 
 @application.route("/")
 def index():
     return "The Flask App Works!"
 
-@application.route('/discover/:username/',methods = ['POST'])
-def test():
-    return "This is /discover/:username/"
-
-@application.route('/discover/post',methods = ['POST'])
-def test_1():
-    #s = request.form
-    return "This is /discover/post"
-
-@application.route("/discover/:username/:newsid",methods = ['GET'])
-def test_2():
-    return "test"
+@app.route('/discover/<username>/<news_id>',methods = ['GET'])
+def get_news(username, query):
+    res = RDBService.get_by_name_id(username, news_id)
+    print(res)
+    return_res = {'username': username, 'news': { 'news_id': news_id, 'content_full': res[len(res) - 1]['full_content'], 'comments':[] } }
+    for i in range(len(res)-1):
+        dict = {'username': res[i]['username'], 'comment_info': res[i]['comment_info']}
+        return_res['news']['comments'].append(dict)
+    print(return_res)
+    rsp = Response(json.dumps(return_res), status=200, content_type="application/json")
+    return rsp
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='127.0.0.1')
-    application.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
