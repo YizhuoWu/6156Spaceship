fcc52de3fc4bd7b8d643aba0437883d90a63b417
Yizhuo Wu <yizhuw1@uci.edu>
2021-12-05 18:35:48 -0500

    updated populate_data.py

---
+++ news-population/RDB_Application/context.py
@@ -0,0 +1,32 @@
+
+import os
+
+# This is a bad place for this import
+import pymysql
+
+def get_db_info(dbname):
+    """
+    This is crappy code.
+
+    :return: A dictionary with connect info for MySQL
+    """
+    # db_host = os.environ.get("DBHOST", None)
+    # db_user = os.environ.get("DBUSER", None)
+    # db_password = os.environ.get("DBPASSWORD", None)
+
+    if dbname == "news":
+        db_info = {
+            "host": "news-db.czclpd5rbl1a.us-east-1.rds.amazonaws.com",
+            "user": "admin",
+            "password": "Fall6156!",
+            "cursorclass": pymysql.cursors.DictCursor
+        }
+    else:
+        db_info = {
+            "host": "comments-db.czclpd5rbl1a.us-east-1.rds.amazonaws.com",
+            "user": "admin",
+            "password": "Fall6156!",
+            "cursorclass": pymysql.cursors.DictCursor
+        }
+
+    return db_info
