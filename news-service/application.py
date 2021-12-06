de3054447f8e32f6158d3ad57e6a6e2a5bae1132
Yizhuo Wu <yizhuw1@uci.edu>
2021-12-05 20:34:24 -0500

    Update application.py

--- news-service/application.py
+++ news-service/application.py
@@ -13,6 +13,38 @@ def index():
     return "The Flask App Works!"
 
 '''
+Get a list of corresponding news with specific label via querying news RDS
+'''
+@app.route('/news',methods = ['GET'])
+def get_news():
+    total_result = 10 # -->TBD
+    labels = request.args.get('labels')
+    labels = eval(labels)
+    news_per_cate = (total_result // len(labels)) + 1 # to evenly get different news by categories
+    result = []
+
+    #fetch news from RDS based on labels
+    for i in range(len(labels)):
+        cur_news_connection = RDBService.get_db_connection("news")
+        label = labels[i]
+        with cur_news_connection:
+            with cur_news_connection.cursor() as cursor:
+                query_sql = "select * from `db-news-schema`.`news_table` where category = " + label
+                query_sql += "LIMIT "
+                query_sql += str(news_per_cate)
+
+                res_news = cursor.execute(query_sql)
+                res_news = cursor.fetchall()
+                result += res_news
+
+    #make result to return
+    json_result = {"totalResults": 10,
+                   "news": result
+                   }
+    rsp = Response(json.dumps(json_result), status=200, content_type="application/json")
+    return rsp
+
+'''
 Given an specific news id, return the news with detailed information.
 Used for extract labels from news.
 '''
@@ -22,7 +54,7 @@ def get_label(news_id):
 
     with cur_news_connection:
         with cur_news_connection.cursor() as cursor:
-            query_sql = "select label from `db-news-schema`.`news_table` where news_id = " + news_id
+            query_sql = "select * from `db-news-schema`.`news_table` where news_id = " + news_id
 
             res_news = cursor.execute(query_sql)
             res_news = cursor.fetchall()
